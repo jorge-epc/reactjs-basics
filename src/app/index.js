699f7f26e0f275810913fff7f0f48c6d43ffb4e3
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2017-07-30 16:29:51 -0500

    Two way binding

--- src/app/index.js
+++ src/app/index.js
@@ -5,17 +5,41 @@ import { Header } from './components/Header';
 import { Home } from './components/Home';
 
 class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      homeLink: 'Home',
+    };
+  }
+
+  onGreet() {
+    alert('Hello!');
+  }
+
+  onChangeLinkName(newName) {
+    this.setState({
+      homeLink: newName,
+    });
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-12">
-            <Header />
+            <Header homeLink={this.state.homeLink} />
           </div>
         </div>
         <div className="row">
           <div className="col-sm-12">
-            <Home name={'Max'} initialAge={10} />
+            <Home
+              name={'Max'}
+              initialAge={10}
+              greet={this.onGreet}
+              changeLink={newName => this.onChangeLinkName(newName)}
+              initialLinkName={this.state.homeLink}
+            />
           </div>
         </div>
       </div>
